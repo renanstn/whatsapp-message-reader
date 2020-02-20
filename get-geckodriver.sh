09eff41005532d2b9d761a4ae03746823620111e
Renan Santana Desiderio <renan_ota@hotmail.com>
2020-02-20 16:17:16 -0300

    Update get-geckodriver.sh

--- get-geckodriver.sh
+++ get-geckodriver.sh
@@ -1,4 +1,5 @@
 # Script que automaticamente já baixa e extrai o geckodriver na pasta bin
 wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz;
 tar -xf geckodriver-v0.26.0-linux64.tar.gz;
+mkdir bin;
 mv geckodriver ./bin;
