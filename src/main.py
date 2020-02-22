6b340ee2c2daea66b24a3630e717a500ee8088c6
Renan Santana <renan.santana@instruct.com.br>
2020-02-22 14:39:02 -0300

    Gitignore atualizado; Adicionada dependência do gtts; Código preparado para usar tanto o chromedriver quando o geckodriver

--- src/main.py
+++ src/main.py
@@ -6,11 +6,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
 
-dir_path = os.getcwd()
-path_geckodriver = os.path.join(dir_path, 'bin', 'geckodriver')
+def get_geckodriver():
+    path_geckodriver = os.path.join(dir_path, 'bin', 'geckodriver')
+    driver = webdriver.Firefox(executable_path=path_geckodriver)
+    return driver
 
-driver = webdriver.Firefox(executable_path=path_geckodriver)
+def get_chromedriver():
+    path_chromedriver = os.path.join(dir_path, 'bin', 'chromedriver')
+    options = webdriver.ChromeOptions()
+    options.add_argument('--user-data-dir=./User_Data')
+    driver = webdriver.Chrome(executable_path=path_chromedriver, chrome_options=options)
+    return driver
+
+
+dir_path = os.getcwd()
 url = "https://web.whatsapp.com/"
+driver = get_chromedriver()
+# driver = get_geckodriver()
 driver.get(url)
 
 try:
@@ -23,6 +35,7 @@ try:
     messages_count_b = messages_count_a
 
     while True:
+        # Notificações de mensagens novas tem div com class '_1ZMSM'
         messages = driver.find_elements_by_class_name('message-in')
         messages_count_b = len(messages)
         if messages_count_a != messages_count_b:
